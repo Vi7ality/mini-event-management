b3b1413105a1d2bfb10eff9d8eabf76cb9e908cb
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-24 11:58:48 +0300

    Router enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home';
+import SharedLayout from './shared/SharedLayout';
+import EventPage from './pages/EventPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="/:eventId" element={<EventPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
