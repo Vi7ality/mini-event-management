05808445cae5e420f1253f4ba7003ca9d0a29a4c
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-24 11:16:45 +0300

    React router dom added

---
+++ src/shared/SharedLayout/index.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+const SharedLayout = () => {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        maxHeight: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      {/* <Header /> */}
+      <main>
+        <Suspense>
+          <Outlet />
+          {/* <ScrollToTop /> */}
+        </Suspense>
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default SharedLayout;
