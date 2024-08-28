30e270c94287647581b3a76fbb94bc250af5875e
Vitalii S. <svitalii138@gmail.com>
2024-08-29 00:10:39 +0300

    filter added to tha table header

--- src/pages/Home/components/EventsTable/index.tsx
+++ src/pages/Home/components/EventsTable/index.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TableHeader from '../TableHeader';
 import styles from './Table.module.scss';
 import Container from '../../../../shared/Container';
 import TableRow from '../TableRow';
 import { useGetAllEventsQuery } from '../../../../services/api';
+import { eventType } from '../../../../services/types';
 
 const EventsTable: React.FC = () => {
   const { data, isLoading } = useGetAllEventsQuery();
+
+  const [sortConfig, setSortConfig] = useState<{
+    key: keyof eventType;
+    direction: 'asc' | 'desc';
+  } | null>(null);
+
+  const sortedData = React.useMemo(() => {
+    if (!sortConfig || !data) return data;
+    const sortedEvents = [...data];
+    sortedEvents.sort((a, b) => {
+      const valueA = a[sortConfig.key];
+      const valueB = b[sortConfig.key];
+
+      if (valueA < valueB) {
+        return sortConfig.direction === 'asc' ? -1 : 1;
+      }
+      if (valueA > valueB) {
+        return sortConfig.direction === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+    return sortedEvents;
+  }, [data, sortConfig]);
+
+  const handleSort = (key: keyof eventType) => {
+    let direction: 'asc' | 'desc' = 'asc';
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === 'asc'
+    ) {
+      direction = 'desc';
+    }
+    setSortConfig({ key, direction });
+  };
 
   if ((!isLoading && !data) || data?.length === 0) {
     return (
@@ -19,9 +55,9 @@ const EventsTable: React.FC = () => {
   return (
     <Container>
       <table className={styles.table}>
-        <TableHeader />
+        <TableHeader handleSort={handleSort} />
         <tbody>
-          {data?.map(event => (
+          {sortedData?.map(event => (
             <TableRow
               key={event.id}
               eventName={event.name}
--- src/pages/Home/components/TableHeader/index.tsx
+++ src/pages/Home/components/TableHeader/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
 import styles from './TableHeader.module.scss';
 
-const TableHeader: React.FC = () => {
+interface ITableHeader {
+  handleSort(arg: string): void;
+}
+
+const TableHeader = ({ handleSort }: ITableHeader) => {
   return (
     <thead className={styles.tableHeader}>
       <tr>
-        <th>Назва події</th>
-        <th>Дата та час</th>
-        <th>Категорія</th>
-        <th>Кількість квитків</th>
-        <th>Вартість</th>
+        <th className={styles.filter} onClick={() => handleSort('name')}>
+          Назва події
+        </th>
+        <th className={styles.filter} onClick={() => handleSort('date')}>
+          Дата та час
+        </th>
+        <th className={styles.filter} onClick={() => handleSort('category')}>
+          Категорія
+        </th>
+        <th className={styles.filter} onClick={() => handleSort('tickets')}>
+          Кількість квитків
+        </th>
+        <th className={styles.filter} onClick={() => handleSort('price')}>
+          Вартість
+        </th>
         <th>Дії</th>
       </tr>
     </thead>
